54a2fdc05963dc775d6afcb950f063cf8510b39f
shawn102400 <shawn102400@hotmail.com>
2020-12-29 22:51:12 +0800

    master good

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,70 @@
 const { series, parallel, watch } = require('gulp');
 const { src, dest } = require('gulp');
 const webserver = require('gulp-webserver');
+const del = require('delete');
 
 function copyHtml() {
     src('src/**/*.vue')
-    .pipe(dest('dist/'));
+        .pipe(dest('dist/'));
     return src('src/**/*.html')
-    .pipe(dest('dist/'));
+        .pipe(dest('dist/'));
 }
 
 function copyCss() {
     return src('src/**/*.css')
-    .pipe(dest('dist/'));
+        .pipe(dest('dist/'));
 }
 
 function copyJs() {
     return src('src/**/*.js')
-    .pipe(dest('dist/'));
+        .pipe(dest('dist/'));
 }
+
 function copyRes() {
-   src('src/**/*.ttf')
-  .pipe(dest('dist/'));
+    src('src/**/*.ttf')
+        .pipe(dest('dist/'));
 
-  src('src/**/*.woff')
-  .pipe(dest('dist/'));
+    src('src/**/*.woff')
+        .pipe(dest('dist/'));
 
+    src('src/**/*.woff2')
+        .pipe(dest('dist/'));
 }
 
 // `clean` 函数并未被导出（export），因此被认为是私有任务（private task）。
 // 它仍然可以被用在 `series()` 组合中。
 function clean(cb) {
-  // body omitted
-  cb();
+    // body omitted
+    del(['dist/*'], cb);
 }
 
 // `build` 函数被导出（export）了，因此它是一个公开任务（public task），并且可以被 `gulp` 命令直接调用。
 // 它也仍然可以被用在 `series()` 组合中。
 function build(cb) {
-  // body omitted
-  copyHtml();
-  copyCss();
-  copyJs();
-  copyRes();
-  cb();
+    // body omitted
+    copyHtml();
+    copyCss();
+    copyJs();
+    copyRes();
+    cb();
 }
+
+watch('src/**/*.html', copyHtml);
+watch('src/**/*.js', copyJs);
+watch('src/**/*.css', copyCss);
 
 function server(cb) {
     // body omitted
-    src('dist').pipe(webserver({    //该任务调用的模块
-        path:'/',
-        host:'127.0.0.1',
-        port:'1357',
+    src('dist').pipe(webserver({ //该任务调用的模块
+        path: '/',
+        host: '127.0.0.1',
+        port: '1357',
         livereload: true, //自动刷新
-        direactoryListing:true, //指定目录找到
-        open:true   //自动打开
+        direactoryListing: true, //指定目录找到
+        open: true //自动打开
     }));
     cb();
-  }
+}
 
 exports.build = build;
-exports.default = series(clean, build,server);
+exports.default = series(clean, build, server);
--- src/asserts/js/index.js
+++ src/asserts/js/index.js
@@ -9,12 +9,16 @@ const Bar = { template: '<div>bar</div>' }
 // 我们晚点再讨论嵌套路由。
 const routes = [{
         path: '/foo',
-        component: () => {
-            //var template = gmallComponent('/module/foo/foo.html');
-            return getVueJs('/module/foo/foo.js');
-        }
+        component: Foo
     },
-    { path: '/bar', component: Bar }
+    {
+        path: '/table',
+        component: httpVueLoader('/module/table.vue')
+    },
+    {
+        path: '/form',
+        component: httpVueLoader('/module/form.vue')
+    }
 ]
 
 // 3. 创建 router 实例，然后传 `routes` 配置
@@ -23,72 +27,10 @@ const router = new VueRouter({
     routes // (缩写) 相当于 routes: routes
 })
 
+
 // 4. 创建和挂载根实例。
 // 记得要通过 router 配置参数注入路由，
 // 从而让整个应用都有路由功能
 const app = new Vue({
     router
 }).$mount('#app')
-
-function getVueTemplate(url) {
-    axios.get(url)
-        .then(function(resp) {
-
-            console.log(resp.data);
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
-}
-
-function getVueJs(url) {
-
-    axios.get(url)
-        .then(function(resp) {
-            console.log("resp.data", resp.data)
-            var f = new Function("return" + " " + resp.data)();
-            console.log("f", f)
-            console.log(resp.data);
-            return f;
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
-
-
-
-}
-
-function gmallComponent(url, vuecom) {
-    if (!vuecom) vuecom = {};
-    axios.get(url).then(function(r) {
-        var rl = r;
-        var style = '';
-        var styleIndexEnd = rl.lastIndexOf('</style>');
-        var styleIndex = rl.lastIndexOf('<style', styleIndexEnd);
-        if (styleIndexEnd !== -1 && styleIndex !== -1) {
-            style = r.substring(styleIndex, styleIndexEnd);
-            style = style.substr(style.indexOf('>') + 1);
-            style = '<component scoped :is="\'style\'">' + style + '</component>';
-        }
-
-        var scriptIndexEnd = rl.lastIndexOf('<\/script>');
-        var scriptIndex = rl.lastIndexOf('<script', scriptIndexEnd);
-        if (scriptIndexEnd !== -1 && scriptIndex !== -1) {
-            var script = r.substring(scriptIndex, scriptIndexEnd);
-            script = script.substr(script.indexOf('>') + 1);
-            script = '(' + script.replace(/export\s*=\s*\{/i, '{') + ')';
-            var obj = eval(script);
-            for (var a in obj) vuecom[a] = obj[a];
-        }
-        var template = r.substring(0, Math.min(styleIndex, scriptIndex));
-        if (style) {
-            var index = template.lastIndexOf('</');
-            if (index !== -1) template = template.substr(0, index) + style + template.substr(index);
-        }
-        vuecom.template = template;
-        debugger
-        resolve(vuecom);
-    });
-
-}
