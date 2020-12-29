4c678e8a4111928dd90a4ee91feabd04979c98e3
shawn102400 <shawn102400@hotmail.com>
2020-12-30 01:42:33 +0800

    xxxx

--- src/module/table.vue
+++ src/module/table.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    <el-table :data="tableData" style="width: 100%">
+    <el-table  :data="tableData" style="width: 100%" stripe border>
         <el-table-column prop="date" label="日期" width="180">
         </el-table-column>
         <el-table-column prop="name" label="姓名" width="180">
